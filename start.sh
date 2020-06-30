ab8a6e2ae6db7dffc2ed4b5dffcf3a1adc03a0a8
peyman <lord.akinak@gmail.com>
2020-06-30 16:02:59 +0430

    livestream implemented; many traefik improvments in routing; mariadb config tube;

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 docker container stop $(docker container ls -aq)
 
-docker-compose -f ./docker-compose.yml --project-director=./ up -d apache2 redis mariadb php-fpm influxdb laravel-echo traefik
+docker-compose -f ./docker-compose.yml --project-director=./ up -d apache2 redis mariadb php-fpm influxdb laravel-echo livestream traefik
