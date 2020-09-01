4cf58c9a60630be29f25f890b0eae1c6ed1b34d6
peyman <lord.akinak@gmail.com>
2020-09-01 09:15:21 +0430

    fix for laravel-echo

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ docker container stop $(docker container ls -aq)
 
 docker-compose -f ./docker-compose.yml --project-director=./ up -d \
     apache2 redis mariadb php-fpm \
-    influxdb livestream telegraf \
+    influxdb telegraf \
     laravel-echo \
     traefik
-#    revslider-apache2 revslider-php-fpm \
+#    livestream revslider-apache2 revslider-php-fpm \
 #    laravel-scheduler
